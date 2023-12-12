964221c4c57d250721b47f49308e3f593aae81c6
Marshall Morrison <you@example.com>
2023-12-12 00:51:42 -0600

    fix ./App.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ProductListings from "./components/ProductListings";
 import ProductPage from "./components/ProductPage";
-import "App.css";
+import "./App.css";
 
 const App = () => {
   return (
