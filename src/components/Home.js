1d7eb7d8b503b77cc273d68a9f976647dcdc036b
Marshall Morrison <you@example.com>
2023-12-13 16:57:36 -0600

    hello

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,30 @@
 // Home.js
 import React from "react";
 import { Link } from "react-router-dom";
-import { initializeApp } from "firebase/app";
+import { initializeApp, getApps } from "firebase/app";
 import "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 
 const Home = () => {
   // Configure Firebase with your own credentials
   const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
+    apiKey: "AIzaSyDKqeqce0HSnhZxCgr686Di0abVg__gRtU",
+    authDomain: "second-home-faf97.firebaseapp.com",
+    databaseURL: "https://second-home-faf97-default-rtdb.firebaseio.com",
+    projectId: "second-home-faf97",
+    storageBucket: "second-home-faf97.appspot.com",
+    messagingSenderId: "776546997812",
+    appId: "1:776546997812:web:6bf6cdd82634a31a5fdc61",
+    measurementId: "G-8LDN5EFM7N",
   };
 
   // Initialize Firebase
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
   }
+
+  const app =
+    getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
   const analytics = getAnalytics(app);
 
